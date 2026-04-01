0bd7bed86d51f513195f54582cc5c8076f5e9818
Qurban <qurbanhanif120@gmail.com>
2026-04-01 10:09:11 +0500

    changes revised

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
     <BrowserRouter>
       <div className='relative z-0 bg-primary w-full min-h-screen overflow-hidden'>
         <Navbar />
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center w-full relative'>
+        <div className='w-full relative'>
           <Hero />
         </div>
         <About />
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,9 +3,9 @@ import ComputersCanvas from "./canvas/Computers";
 
 const Hero = () => {
   return (
-    <section className="relative w-full h-screen mx-auto overflow-hidden bg-primary">
+    <section className="relative w-full min-h-screen mx-auto overflow-hidden bg-primary isolate">
       <div className="absolute inset-0 bg-hero-pattern bg-cover bg-no-repeat bg-center opacity-90" />
-      {/* <div className="absolute inset-0 bg-gradient-to-b from-primary/95 via-primary/80 to-transparent z-10" /> */}
+      <div className="absolute inset-0 bg-gradient-to-b from-primary/95 via-primary/80 to-transparent z-10" />
       <div className={`absolute inset-0 top-[100px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5 z-30 pointer-events-none`}>
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915EFF]" />

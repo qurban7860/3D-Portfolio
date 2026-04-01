e9478ab9f7884fe0df3601382a2e52a5c45c33b8
Qurban <qurbanhanif120@gmail.com>
2026-04-01 12:05:01 +0500

    set canva position

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,7 +23,7 @@ const Hero = () => {
         </div>
       </div>
 
-      <div className="absolute inset-x-0 bottom-0 h-[45vh] sm:h-[60vh] md:h-[70vh] z-10 pointer-events-none">
+      <div className="absolute inset-x-0 bottom-0 h-[60vh] sm:h-[70vh] z-10 pointer-events-none">
         <ComputersCanvas />
       </div>
     </section>
